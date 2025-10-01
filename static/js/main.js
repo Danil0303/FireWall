6132c69b1180b07ce9d802ff82086c538eb733b8
Пользователь <rumyancev.01@mail.ru>
2025-10-01 16:37:45 +0300

    main

--- static/js/main.js
+++ static/js/main.js
@@ -5,18 +5,17 @@ function Add(event) {
     let ip = document.getElementById('ipv4Input');
     let dateFrom = document.getElementById('dateFrom').value;
     let dateTo = document.getElementById('dateTo').value;
-    if (!isValid(ip.value)) return console.log(ip);
+    if (!isValid(ip.value)) return ShowInfo("IPv4: "+ip.value+" некорректный");
     let ipList = document.querySelector(".ip-container");
-    if (isOneIp(ip.value)) return console.log(ip);
+    if (isOneIp(ip.value)) return ShowInfo("IPv4: "+ip.value+" уже добавлен!");
     let newItem = document.createElement('div');
     newItem.classList.add('ip-list');
     newItem.innerHTML = `
          <span> ${ip.value}</span> <button class="delete-button" onclick="removeIP(this)">Удалить</button>
     `;
     ipList.appendChild(newItem);
+    ShowInfo("IPv4 добавлен: "+ip.value, 'success');
     ip.value = '';
-    return console.log(ip);
-
 }
 function removeIP(button){
        let rem = button.parentNode;
@@ -35,8 +34,12 @@ function isOneIp(ip) {
 
 function ShowInfo(message, type='error'){
         let toast = document.querySelector(".hidden");
-        let newItem = document.createElement('div');
-
+        let toast_div = document.createElement('div');
+        if (type == 'error') toast_div.classList.add('btn-error');
+        toast_div.classList.add('btn');
+        toast_div.innerHTML = `
+         <span> ${message}</span> <button class="close-button" onclick="removeIP(this)">×</button>
+        `;
+        toast.appendChild(toast_div);
+        setTimeout(function(){toast_div.remove()}, 5000);
 }
-
-//×
